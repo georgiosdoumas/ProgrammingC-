48a39093e37280e9208e2f795f167f4bd9a02c40
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-05-27 17:13:19 +0200

    Create 7.6exercise.programming3.c

---
+++ ProblemSolvingAndProgramDesingInC_7ed2013/chap07/7.6exercise.programming3.c
@@ -0,0 +1,46 @@
+/* Exercise programming 3 , page 406 
+Modify the heading and declarations of function select_sort so it would sort an 
+array of type double values. Be careful—some variables should still be of type int ! */
+
+#include <stdio.h>
+int get_index_of_min(const double arr[], int first, int last)  
+{
+	int i, position_of_min = first;
+	for (i = first; i <= last; ++i) 
+	  if(arr[i] < arr[position_of_min]) position_of_min = i;
+	return (position_of_min);
+}
+
+void select_sort(double list[], int listsize)
+{
+	int running_index, index_of_min;
+	double temp;
+	for (running_index = 0; running_index < listsize-1; ++running_index) 
+	{
+		index_of_min = get_index_of_min(list, running_index, listsize-1); 
+		if (running_index != index_of_min) 
+		{
+			temp = list[index_of_min];
+			list[index_of_min] = list[running_index];
+			list[running_index] = temp;
+		}
+	}
+}
+
+void display_arr(double arr[], int arrsize)
+{
+	int i;
+	for(i=0;i<arrsize;i++) printf("%6.2f ",arr[i]);
+	printf("\n");
+}
+
+int main(void)
+{
+	double data1[]={50.1,50.1,47.2,23.7,12.78,9.65,5.44,1.29};
+	double data2[]={7.1,28.22,18,37.6,42.1,42.1,4.23};
+	display_arr(data1,8); display_arr(data2,7); 
+	select_sort(data1, 8);
+	select_sort(data2, 7);
+	display_arr(data1,8); display_arr(data2,7); 
+	return 0;
+} 

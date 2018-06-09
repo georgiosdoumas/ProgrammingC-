3d3a1f28009d758df1cf761fac1b026269628750
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-06-09 10:15:10 +0200

    Create 14.2example14.2fork.c

---
+++ ProblemSolvingAndProgramDesingInC_7ed2013/chap14/14.2example14.2fork.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+  pid_t pid;
+  printf("I am the parent process with ID: %d\n", getpid());
+  printf("Lets create a child process with fork...\n");
+  pid = fork();
+  if (pid < 0)
+  {   /* Code executed in the parent process only if unsuccessful */
+    printf("  Error on attempting to fork and create a child process!\n");
+  }
+  else if (pid == 0)
+  {   /* Code executed in the child process only if successful */
+    printf("    Report form inside the child process: Child ID %d\n", getpid());
+    printf("    Exclusive output from child process here! \n");
+  }
+  else
+  { /* Code executed in the parent process only if successful */
+    printf("  Parent ID %d successfuly created Child ID %d\n", getpid(), pid);
+  }
+  printf("  Where will this get print? From both parent and child %d!\n", getpid());
+  return 0;
+}
+
+// gcc -Wall -o 14.2example14.2 14.2example14.2.c
+
